979155839c47b2f4ba6d0ab1177e79c600522898
mobyw <44370805+mobyw@users.noreply.github.com>
2024-06-02 13:25:15 +0000

    Deploying to gh-pages from @ Mar-7th/March7th-Docs@cbd300d63ec8aab4b08e9f3bd29076f0bcae0a77 🚀

---
+++ assets/structure.html-DQF3rLco.js
@@ -0,0 +1,37 @@
+import{_ as e}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as n,o as i,d as t}from"./app-au9n5wTj.js";const a={},s=t(`<p>本页面介绍资源仓库 <a href="https://github.com/Mar-7th/StarRailRes" target="_blank" rel="noopener noreferrer">Mar-7th/StarRailRes</a> 的目录结构。</p><h2 id="项目结构" tabindex="-1"><a class="header-anchor" href="#项目结构"><span>项目结构</span></a></h2><p>项目文件结构如下：</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>StarRailRes
+├─ icon         # 图标素材目录
+├─ image        # 图片素材目录
+├─ guide        # 教程图片目录
+├─ index_new    # 索引目录（之后会转移到index目录）
+├─ index_min    # 压缩索引目录（压缩换行以更快下载）
+└─ index        # 旧索引目录（不推荐使用）
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>在 <code>index_new</code> 或 <code>index_min</code> 中有各种语言的索引文件夹，支持的语言列表如下：</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>CN    简体中文
+CHT   繁體中文
+DE    Deutsch
+EN    English
+ES    Español
+FR    Français
+ID    Bahasa Indonesia
+JP    日本語
+KR    한국어
+PT    Português
+RU    Русский
+TH    ภาษาไทย
+VI    Tiếng Việt
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>语言文件夹名为对应语言缩写的小写字母形式，如 <code>cn</code> 文件夹为简体中文索引。</p><h2 id="索引结构" tabindex="-1"><a class="header-anchor" href="#索引结构"><span>索引结构</span></a></h2><p>在具体的语言索引文件中，存在以下文件：</p><div class="language-text line-numbers-mode" data-ext="text" data-title="text"><pre class="language-text"><code>paths.json                      # 命途
+elements.json                   # 元素
+properties.json                 # 属性
+characters.json                 # 角色基本信息
+character_ranks.json            # 角色星魂
+character_skills.json           # 角色技能
+character_skill_trees.json      # 角色行迹
+character_promotions.json       # 角色晋阶与等级
+light_cones.json                # 光锥基本信息
+light_cone_ranks.json           # 光锥叠影
+light_cone_promotions.json      # 光锥晋阶与等级
+relics.json                     # 遗器基本信息
+relic_sets.json                 # 遗器套装信息
+relic_main_affixs.json          # 遗器主词条组
+relic_sub_affixs.json           # 遗器副词条组
+items.json                      # 材料
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>各索引文件的具体结构请在对应页面查看。</p>`,11),r=[s];function l(d,c){return i(),n("div",null,r)}const u=e(a,[["render",l],["__file","structure.html.vue"]]),v=JSON.parse('{"path":"/zh/resource/structure.html","title":"目录结构","lang":"zh-CN","frontmatter":{"title":"目录结构","icon":"ph:bookmarks","order":1,"category":["资源"],"description":"本页面介绍资源仓库 Mar-7th/StarRailRes 的目录结构。 项目结构 项目文件结构如下： 在 index_new 或 index_min 中有各种语言的索引文件夹，支持的语言列表如下： 语言文件夹名为对应语言缩写的小写字母形式，如 cn 文件夹为简体中文索引。 索引结构 在具体的语言索引文件中，存在以下文件： 各索引文件的具体结构请在对应...","head":[["link",{"rel":"alternate","hreflang":"en-us","href":"https://march7th.xyz/en/resource/structure.html"}],["meta",{"property":"og:url","content":"https://march7th.xyz/zh/resource/structure.html"}],["meta",{"property":"og:site_name","content":"March7th 文档"}],["meta",{"property":"og:title","content":"目录结构"}],["meta",{"property":"og:description","content":"本页面介绍资源仓库 Mar-7th/StarRailRes 的目录结构。 项目结构 项目文件结构如下： 在 index_new 或 index_min 中有各种语言的索引文件夹，支持的语言列表如下： 语言文件夹名为对应语言缩写的小写字母形式，如 cn 文件夹为简体中文索引。 索引结构 在具体的语言索引文件中，存在以下文件： 各索引文件的具体结构请在对应..."}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:locale:alternate","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2024-05-26T07:37:44.000Z"}],["meta",{"property":"article:author","content":"Mar-7th"}],["meta",{"property":"article:modified_time","content":"2024-05-26T07:37:44.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"目录结构\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-05-26T07:37:44.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Mar-7th\\",\\"url\\":\\"https://github.com/Mar-7th\\"}]}"]]},"headers":[{"level":2,"title":"项目结构","slug":"项目结构","link":"#项目结构","children":[]},{"level":2,"title":"索引结构","slug":"索引结构","link":"#索引结构","children":[]}],"git":{"createdTime":1683961498000,"updatedTime":1716709064000,"contributors":[{"name":"mobyw","email":"44370805+mobyw@users.noreply.github.com","commits":2},{"name":"mobyw","email":"mobyw66@gmail.com","commits":1}]},"readingTime":{"minutes":1.11,"words":333},"filePathRelative":"zh/resource/structure.md","localizedDate":"2023年5月13日","autoDesc":true}');export{u as comp,v as data};
